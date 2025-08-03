924b7a9f6262ad48ef528c8251f2b641d981343a
Ado <minexdt@gmail.com>
2025-08-03 14:22:23 -0600

    Delete api directory

--- api/endpoint1.js
+++
@@ -1,7 +0,0 @@
-import verifyKey from '../utils/verifyKey.js'
-
-export default function handler(req, res) {
-  verifyKey(req, res, () => {
-    res.json({ success: true, message: 'Endpoint 1 funcionando 😎' })
-  })
-}
--- api/endpoint2.js
+++
@@ -1,7 +0,0 @@
-import verifyKey from '../utils/verifyKey.js'
-
-export default function handler(req, res) {
-  verifyKey(req, res, () => {
-    res.json({ success: true, data: [1, 2, 3] })
-  })
-}
